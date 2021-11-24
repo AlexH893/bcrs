d0640d1686f244e31309cdef2286d6d2795f4480
Sarah Jean Baptiste <sjeand@hotmail.com>
2021-11-24 14:03:19 -0600

    updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,8 @@ import { HomeComponent } from './pages/home/home.component';
 import { BaseLayoutComponent } from './shared/base-layout/base-layout.component';
 import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-<<<<<<< HEAD
 import { UserConfigurationComponent } from './pages/user-configuration/user-configuration.component';
-=======
 import { SignInComponent } from './sign-in/sign-in.component';
->>>>>>> fed69fd168b2c222fa20fd110f5b77f8017efb9c
 
 const routes: Routes = [
   {
@@ -18,13 +15,12 @@ const routes: Routes = [
         component: HomeComponent
       },
       {
-<<<<<<< HEAD
         path: 'user-configuration',
         component: UserConfigurationComponent
-=======
+      },
+      {
         path: 'sign-in',
         component: SignInComponent
->>>>>>> fed69fd168b2c222fa20fd110f5b77f8017efb9c
       }
     ]
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,19 +9,15 @@ import { HomeComponent } from './pages/home/home.component';
 import { BaseLayoutComponent } from './shared/base-layout/base-layout.component';
 import { AuthLayoutComponent } from './shared/auth-layout/auth-layout.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatToolbarModule } from '@angular/material/toolbar'; 
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-<<<<<<< HEAD
-import { SigninComponent } from './signin/signin.component';
 import { UserConfigurationComponent } from './pages/user-configuration/user-configuration.component';
-=======
 import { SignInComponent } from './sign-in/sign-in.component';
 import { MatCardModule } from '@angular/material/card';
-import {MatFormFieldModule} from '@angular/material/form-field';
->>>>>>> fed69fd168b2c222fa20fd110f5b77f8017efb9c
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
@@ -29,12 +25,8 @@ import {MatFormFieldModule} from '@angular/material/form-field';
     HomeComponent,
     BaseLayoutComponent,
     AuthLayoutComponent,
-<<<<<<< HEAD
-    SigninComponent,
+    SignInComponent,
     UserConfigurationComponent
-=======
-    SignInComponent
->>>>>>> fed69fd168b2c222fa20fd110f5b77f8017efb9c
   ],
   imports: [
     BrowserModule,
