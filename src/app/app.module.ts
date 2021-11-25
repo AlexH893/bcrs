8d25aabc35d85d9bc2d891184962482fada88ecc
atm951999 <amartin3@woh.rr.com>
2021-11-25 12:01:34 -0500

    Test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,8 +14,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { UserConfigurationComponent } from './pages/user-configuration/user-configuration.component';
 import { SignInComponent } from './sign-in/sign-in.component';
+import { UserConfigurationComponent } from './pages/user-configuration/user-configuration.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 
