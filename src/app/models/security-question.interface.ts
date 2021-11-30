dff3aa8ccbbfd77d2018f82671994a977d139a28
atm951999 <amartin3@woh.rr.com>
2021-11-30 18:33:32 -0500

    Angie 11/30 6:33 PM EST

--- src/app/models/security-question.interface.ts
+++ src/app/models/security-question.interface.ts
@@ -1,3 +1,5 @@
+import { User } from "./user.interface";
+
 export interface SecurityQuestion {
   text: string,
   answer: string,
--- src/app/pages/security-questions/security-questions.component.ts
+++ src/app/pages/security-questions/security-questions.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CreateQuestionDialogComponent } from 'src/app/shared/create-question-dialog/create-question-dialog.component';
 import { MatDialog } from '@angular/material/dialog';
 import { SecurityQuestion } from 'src/app/models/security-question.interface';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-security-questions',
@@ -10,16 +11,24 @@ import { SecurityQuestion } from 'src/app/models/security-question.interface';
 })
 
 export class SecurityQuestionsComponent implements OnInit {
-  securityQuestions: SecurityQuestion[];
-  constructor(private dialog: MatDialog) {}
+  questions: SecurityQuestion[];
+  constructor(public dialog: MatDialog, private http: HttpClient) {}
 
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.fetchQuestions
+  }
 
   //Task dialog to open when user hits button
   openCreateQuestionDialog() {
     const dialogRef = this.dialog.open(CreateQuestionDialogComponent, {
       disableClose: true
+    })
+  }
+
+  fetchQuestions(): void {
+    this.http.get('/api/questions').subscribe((res: SecurityQuestion[]) => {
+      this.questions = res
     })
   }
 
