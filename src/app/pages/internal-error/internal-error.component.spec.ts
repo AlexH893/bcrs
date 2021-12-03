edc9e1365c927c5522ac82ad5af847188c11d22d
atm951999 <amartin3@woh.rr.com>
2021-12-03 17:41:02 -0500

    Angie - Syncing - 12/3 5:40 PM EST

---
+++ src/app/pages/internal-error/internal-error.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { InternalErrorComponent } from './internal-error.component';
+
+describe('InternalErrorComponent', () => {
+  let component: InternalErrorComponent;
+  let fixture: ComponentFixture<InternalErrorComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ InternalErrorComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(InternalErrorComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
