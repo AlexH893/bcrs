8aa77237f84c603ce4bc31418488778f35cd75ff
Sarah Jean Baptiste <sjeand@hotmail.com>
2021-12-08 15:29:21 -0600

    invoice dialog

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { InvoiceDialogComponent } from '../invoice-dialog/invoice-dialog.component';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
   }
 
+  // open dialog to display selected services, parts, labor and total of invoice
+  openDialog(): void {
+    const dialogRef = this.dialog.open(InvoiceDialogComponent)
+  };
 }
+
