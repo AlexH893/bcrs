bd10c8eda66f639aeba1cba6d846661079bb6ef9
Alex <31377763+AlexH893@users.noreply.github.com>
2021-12-09 22:23:31 -0700

    update role model

--- server/models/role.js
+++ server/models/role.js
@@ -11,15 +11,9 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-let roleSchema = new Schema({
-  /*  role: { type: String, default: "standard" },*/
+const roleSchema = new Schema({
   text: { type: String, unique: true },
   isDisabled: { type: Boolean, default: false },
 });
 
-const RoleDocument = mongoose.model("Role", roleSchema);
-
-module.exports = {
-  roleSchema,
-  RoleDocument,
-};
+module.exports = mongoose.model("Role", roleSchema);
