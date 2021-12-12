639a528a3f71bcc62555da32e6379afea93df8ec
Alex <31377763+AlexH893@users.noreply.github.com>
2021-12-12 13:19:39 -0700

    adding line item schema

---
+++ server/models/line-item.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const lineItemSchema = new Schema({
+  title: { type: String },
+  price: { type: Number },
+})
+
+module.exports = lineItemSchema;
