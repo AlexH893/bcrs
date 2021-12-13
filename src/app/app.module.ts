6a1a533a35ecbc902f24f3a2963acdc75299914f
Alex <31377763+AlexH893@users.noreply.github.com>
2021-12-13 16:56:42 -0700

    purchases by service graph component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,3 @@
-/*
-============================================
-; Title: Bobs Computer Repair Shop
-; Author: Professor Krasso
-; Date: 27 November 2021
-; Modified By: Angela Martin, Alex Haefner & Sarah Jean Baptiste
-; Description: User-config Component
-==========================================
-*/
-
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
@@ -31,12 +21,15 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { SecurityQuestionsComponent } from './pages/security-questions/security-questions.component';
 import { MatTabsModule } from '@angular/material/tabs';
 import { CreateUserComponent } from './pages/create-user/create-user.component';
-import { MatDialogModule, MAT_DIALOG_DEFAULT_OPTIONS } from '@angular/material/dialog';
+import {
+  MatDialogModule,
+  MAT_DIALOG_DEFAULT_OPTIONS,
+} from '@angular/material/dialog';
 import { CookieService } from 'ngx-cookie-service';
 import { VerifySecurityQuestionsComponent } from './pages/verify-security-questions/verify-security-questions.component';
 import { CommonModule } from '@angular/common';
 import { CreateQuestionDialogComponent } from './shared/create-question-dialog/create-question-dialog.component';
-import { MatInputModule } from '@angular/material/input'
+import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatSelectModule } from '@angular/material/select';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
@@ -57,7 +50,6 @@ import { DatePipe } from '@angular/common';
 import { RoleConfigurationComponent } from './pages/role-configuration/role-configuration.component';
 import { MatMenuModule } from '@angular/material/menu';
 import { RoleCreateEditComponent } from './pages/role-create-edit/role-create-edit.component';
-import { PurchasesByServiceGraphComponent } from './purchases-by-service-graph/purchases-by-service-graph.component';
 
 @NgModule({
   declarations: [
@@ -82,7 +74,6 @@ import { PurchasesByServiceGraphComponent } from './purchases-by-service-graph/p
     InvoiceDialogComponent,
     RoleConfigurationComponent,
     RoleCreateEditComponent,
-    PurchasesByServiceGraphComponent
   ],
   imports: [
     BrowserModule,
@@ -109,13 +100,13 @@ import { PurchasesByServiceGraphComponent } from './purchases-by-service-graph/p
     MatTableModule,
     MatGridListModule,
     MatListModule,
-    MatMenuModule
+    MatMenuModule,
   ],
   providers: [
     CookieService,
     DatePipe,
-    {provide: MAT_DIALOG_DEFAULT_OPTIONS, useValue: {hasBackdrop: false}}
+    { provide: MAT_DIALOG_DEFAULT_OPTIONS, useValue: { hasBackdrop: false } },
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
