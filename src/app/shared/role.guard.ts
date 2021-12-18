0baab42c44556b75d0319a9e2c548c58aaf87bd5
Alex <31377763+AlexH893@users.noreply.github.com>
2021-12-17 18:24:45 -0600

    Update role.guard.ts

--- src/app/shared/role.guard.ts
+++ src/app/shared/role.guard.ts
@@ -1,33 +1,47 @@
-
-
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+  Router,
+} from '@angular/router';
 import { Observable } from 'rxjs';
-import { CookieService } from 'ngx-Cookie-service';
+import { CookieService } from 'ngx-cookie-service';
 import { map } from 'rxjs/operators';
 import { RoleService } from 'src/services/role.service';
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class RoleGuard implements CanActivate {
-
-  constructor(private router: Router, private cookieService: CookieService, private roleService: RoleService) {}
+  constructor(
+    private router: Router,
+    private cookieService: CookieService,
+    private roleService: RoleService
+  ) {}
 
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-      return this.roleService.findUserRole(this.cookieService.get('session_user')).pipe(map(res => {
-        console.log(res);
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    return this.roleService
+      .findUserRole(this.cookieService.get('session_user'))
+      .pipe(
+        map((res) => {
+          console.log(res);
 
-      if (res.text === 'admin') {
-        return true;
-      } else {
-        this.router.navigate(['/']);
-        return false;
-      }
-    }))
+          if (res.text === 'admin') {
+            return true;
+          } else {
+            this.router.navigate(['/']);
+            return false;
+          }
+        })
+      );
   }
 }
